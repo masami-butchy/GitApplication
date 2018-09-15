f7ab3a507d2a481c901591a38200b3285c1e767c
masami-butchy <m.physics21chemistry.scientist@gmail.com>
2018-09-15 10:45:38 +0900

    SecoundCommit

--- app/src/main/java/android/lifeistech/com/gitapplication/MainActivity.java
+++ app/src/main/java/android/lifeistech/com/gitapplication/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int lit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lit = 0;
     }
 }
